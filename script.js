539623b7192207d7e035e72c7edb04a00db8db19
JOELHERMES <joelhermesferrufino@gmail.com>
2026-08-16 00:47:24 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function mostrarMensaje() {
-    const globo = document.getElementById('globo');
-    
-    // Muestra u oculta el mensaje al hacer clic sobre la chica
-    globo.classList.toggle('oculto');
-    globo.classList.toggle('visible');
-}
