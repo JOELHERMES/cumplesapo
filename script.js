46056771e62e35ae9788f95038c5f462c9236087
JOELHERMES <joelhermesferrufino@gmail.com>
2026-08-15 22:26:22 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function disparar() {
-    const kurumi = document.getElementById('kurumi');
-    kurumi.classList.add('animar');
-
-    setTimeout(() => {
-        document.getElementById('mensaje').style.display = 'block';
-    }, 1000); // 1000 milisegundos = 1 segundo
-}
